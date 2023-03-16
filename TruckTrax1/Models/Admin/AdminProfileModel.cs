7c7d8f6242722bba4087d60e434007e5af60f138
LUKE BAIERL <lukeest1984@gmail.com>
2023-03-16 14:38:37 -0500

    Added Customer and admin profile models.

---
+++ TruckTrax1/Models/Admin/AdminProfileModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruckTrax1.Models.Admin
+{
+    public class AdminProfile
+    {
+    }
+}
---
+++ TruckTrax1/Models/Customer/CustomerProfileModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruckTrax1.Models.Customer
+{
+    public class CustomerProfile
+    {
+    }
+}
